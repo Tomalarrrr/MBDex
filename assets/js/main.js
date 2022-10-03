10d04530b08b43886fcf2d6c4a9942be7af265a5
Tomalarrrr <105936702+Tomalarrrr@users.noreply.github.com>
2022-10-03 16:48:22 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,96 @@
+(function() {
+    
+    "use strict";
+    
+
+    
+    /*=====================================
+    Sticky
+    ======================================= */
+    window.onscroll = function () {
+        var header_navbar = document.getElementById("header_navbar");
+        var logo = document.querySelector("img#logo");
+        var sticky = header_navbar.offsetTop;
+
+        if (window.pageYOffset > sticky) {
+            header_navbar.classList.add("sticky");
+            logo.setAttribute("src", "assets/images/logo.png")
+        } else {
+            header_navbar.classList.remove("sticky");
+            logo.setAttribute("src", "assets/images/logo.png")
+        }
+
+
+
+        // show or hide the back-top-top button
+        var backToTo = document.querySelector(".back-to-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "block";
+        } else {
+            backToTo.style.display = "none";
+        }
+    };
+
+        //===== close navbar-collapse when a  clicked
+        let navbarToggler = document.querySelector(".navbar-toggler");    
+        var navbarCollapse = document.querySelector(".navbar-collapse");
+    
+        document.querySelectorAll(".page-scroll").forEach(e =>
+            e.addEventListener("click", () => {
+                navbarToggler.classList.remove("active");
+                navbarCollapse.classList.remove('show')
+            })
+        );
+
+
+    // for menu scroll 
+    var pageLink = document.querySelectorAll('.page-scroll');
+    
+    pageLink.forEach(elem => {
+        elem.addEventListener('click', e => {
+            e.preventDefault();
+            document.querySelector(elem.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                offsetTop: 1 - 60,
+            });
+        });
+    });
+
+    // section menu active
+    function onScroll(event) {
+        var sections = document.querySelectorAll('.page-scroll');
+        var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+
+        for (var i = 0; i < sections.length; i++) {
+            var currLink = sections[i];
+            var val = currLink.getAttribute('href');
+            var refElement = document.querySelector(val);
+            var scrollTopMinus = scrollPos + 73;
+            if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
+                document.querySelector('.page-scroll').classList.remove('active');
+                currLink.classList.add('active');
+            } else {
+                currLink.classList.remove('active');
+            }
+        }
+    };
+
+    window.document.addEventListener('scroll', onScroll);
+
+
+    //===== glide slider for testimonial
+    
+    new Glide('.glide', {
+        type: 'slider',
+        perView: 1,
+        animationDuration: 1000
+    }).mount()
+
+    //WOW Scroll Spy
+    var wow = new WOW({
+        //disabled for mobile
+        mobile: false
+    });
+    wow.init();
+    
+})();
